55f88dbf7843cf6cdb41d4bfcea53019af7ad1fa
TSaker95 <torryarno@hotmail.com>
2019-07-30 17:48:32 +1000

    Doing a quick update, things are coming along smoothly and im quite happy with progres so far!

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Footer from "./components/Footer.js";
 import SideBar from "./components/sidebar.js";
 import symbol from "./images/symbolone.png";
 import Slideshow from "./components/Slideshow.js";
+import art from "./images/Artofthenation.png";
 
 function App() {
   return (
@@ -15,75 +16,37 @@ function App() {
 
       <div className="pagemargin">
         <div className="header">
-          <h1> WELCOME TO A NEW CIVILISATION </h1>{" "}
+          <h1 className="h1"> "Welcome To A New Civilisation" </h1>
         </div>
 
-        <div className="section">
+        <div className="sectionslideshow">
           <Slideshow />
-
-          {/* <img className="homescreenimg " src={symbol} alt="Logo" /> */}
         </div>
         <div className="section">
-          <h1> Introduction </h1>
-          <p>
-            Welcome to the website of my book: A New Civilisation! Click the
-            links above to explore the Universe of my story
-          </p>
+          <div className="sectiontext">
+            <p>
+              Introduction: <br /> <br /> Welcome to the website of my book: 'A
+              New Civilisation'. Here you can check out some of the book related
+              art and projects I have made, as well as explore 'The Nation'. The
+              new world order of the 22nd century!
+            </p>
+          </div>
         </div>
         <div className="header">
-          <h1> Follow my stuff </h1>{" "}
+          <h1 className="h1"> Explore The Nation </h1>
         </div>
         <div className="section">
-          <h1> What </h1>
-        </div>
-        <div className="section">
-          <h1> What </h1>
-        </div>
-
-        <div className="section">
-          <h1> What </h1>
-        </div>
-        <div className="section">
-          <h1> What </h1>
-        </div>
-
-        <div className="section">
-          <h1> What </h1>
-        </div>
-        <div className="section">
-          <h1> What </h1>
-        </div>
-
-        <div className="section">
-          <h1> What </h1>
-        </div>
-        <div className="section">
-          <h1> What </h1>
-        </div>
-
-        <div className="section">
-          <h1> What </h1>
-        </div>
-        <div className="section">
-          <h1> What </h1>
-        </div>
-
-        <div className="section">
-          <h1> What </h1>
-        </div>
-        <div className="section">
-          <h1> What </h1>
-        </div>
-
-        <div className="section">
-          <h1> What </h1>
-        </div>
-        <div className="section">
-          <h1> What </h1>
-        </div>
-
-        <div className="section">
-          <h1> What </h1>
+          <div className="sectionhalf">
+            <h1> Art of The Nation</h1>
+            <img className="imgart" src={art} alt="Logo" />
+            <p className="sectiontext">
+              Check out my story related projects including: <br />
+              - 3D Printing
+              <br />
+              - Drawings <br />- Cosplays
+            </p>
+          </div>
+          <div className="sectionhalf"> what </div>
         </div>
       </div>
       <Footer />
