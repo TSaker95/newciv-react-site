346c98c0716a97cdb34db8a3e2f8d2bcaa71df04
TSaker95 <torryarno@hotmail.com>
2019-07-31 11:34:30 +1000

    fixed up footer with some images and text, small update here. but it is fairly flexable though needs a tweeking for mobile mode with the font size. other than that looks ok. might pretty it up a bit later

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,20 @@
 import React from "react";
+import facebook from "../images/facebookicon.png";
+import instagram from "../images/instagram.png";
 
 export default function Footer() {
   return (
     <footer>
-      <div className="footer"> this is a footer</div>
+      <div className="footer">
+        <p className="footertext"> &copy; A New Civilisation 2019 </p>
+        <p className="footertext"> By: Torryn Saker </p>
+        <p className="footertext">
+          <img id="imgfacebookicon" src={facebook} alt="Logo" />
+        </p>
+        <p className="footertext">
+          <img id="imgfacebookicon" src={instagram} alt="Logo" />
+        </p>
+      </div>
     </footer>
   );
 }
