7e81e6d0f9416718de62c6dbed499beea37f07c8
TSaker95 <torryarno@hotmail.com>
2019-08-01 11:23:10 +1000

    fixed most problems with mobile mode, looks fine again!

--- src/App.js
+++ src/App.js
@@ -24,17 +24,18 @@ function App() {
         <div className="sectionslideshow">
           <Slideshow />
         </div>
-        <div className="section">
-          <div className="sectiontext">
-            <p>
-              Welcome to the website of my book: 'A New Civilisation'. Here you
-              can check out some of the book related art and projects I have
-              made, as well as explore 'The Nation'. The new world order of the
-              22nd century!
-            </p>
+        <div className="sectionslideshow">
+          <div className="section">
+            <div className="sectiontext">
+              <p>
+                Welcome to the website of my book: 'A New Civilisation'. Here
+                you can check out some of the book related art and projects I
+                have made, as well as explore 'The Nation'. The new world order
+                of the 22nd century!
+              </p>
+            </div>
           </div>
         </div>
-
         <div className="header">
           <h1 className="h1"> Explore The Nation </h1>
         </div>
